fe2d805fc277d7487a19bbbc4a2e744183ed8b1d
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-03-22 21:59:08 +0530

    database ok

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import telebot
 from telebot import types
+import dataBase
 
 
 bot = telebot.TeleBot("6076557712:AAHiHRMsr68rOUm1-5BjpuDZUf4pmXwWimA")
+
+db = dataBase.DataBase()
 
 menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
 menu.add("/show_products")
@@ -14,6 +17,8 @@ catogory.add('catagory_example','catagory_example','catagory_example', 'catagory
 @bot.message_handler(commands=['start', 'hello'])
 def startbot(message):
     # bot.reply_to(message, "welcome!!!", reply_markup=menu)
+    flag = db.checkUsernameExist(message.chat.id)
+    print(flag)
 
 
 @bot.message_handler(commands=['show_products'])
